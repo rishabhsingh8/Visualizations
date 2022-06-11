0a793f5df7cce9dd80f08803b02062b245e51a8b
Rishabh Singh Ahluwalia <rishabhsingh8@gmail.com>
2022-06-10 21:52:34 -0700

    Add Graph Builder UI

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
 // Include MetricsGraphics library
 //
 import { LineChart, HistogramChart, ScatterChart } from "./index.js";
+window.isCodeShowing = false;
 
+window.configuration = {
+    title: "The Rise of UFO Sightings",
+    description: "According to [reuters](https://www.reuters.com/article/us-britain-ufo/ufo-sightings-may-have-been-down-to-x-files-idUSTRE57G2EU20090817), UFO sightings may be correlated with the rise in popularity of television shows like X-Files.",
+    width: 650,
+    height: 200,
+    dataset: "ufosightings.json",
+    xAccessor: "year",
+    yAccessor: "sightings",
+    area: true,
+    xAxis: {
+        extendedTicks: false,
+        label: "Year",
+        tickFormat: ".4r"
+    },
+    yAxis: {
+        label: "UFO Sightings"
+    },
+    colors: ["#5B75CF", "#22C55D", "#A855F7"],
+    markers: [{ year: 1993, label: "'X-Files' released" }],
+    baselines: [],
+    legend: ['UFO Sightings']
+}
+
+window.configDescription = {
+    title: "Title of the visualization",
+    description: "A blurb to describe context",
+    width: "Total width of the graph",
+    height: "Total height of the graph",
+    dataset: "The dataset to plot. (uploaded above)",
+    xAccessor: "The name of the field in the dataset that contains the X values",
+    yAccessor: "The name of the field in the dataset that contains the Y values",
+    area: "Show an area under the line or not",
+    xAxis: "-",
+    yAxis: "-",
+    colors: "Custom color palette for the graph.",
+    markers: "Markers that should be added to the chart. Each marker object should be accessible through the xAccessor and contain a label field",
+    baselines: "Baselines that should be added to the chart. Each baseline object should be accessible through the yAccessor and contain a label field"
+}
+
+// Get a dataset
 window.getDataset = function GetDataset(filename) {
     var dataset = null;
     window.datasets.forEach(element => {
@@ -13,25 +54,15 @@ window.getDataset = function GetDataset(filename) {
     return dataset;
 }
 
+// Update the graph title
 window.setGraphTitle = function SetGraphTitle(title) {
     document.getElementById('graphTitle').innerText = title;
 }
 
+// Set the description blurb
 window.setGraphDescription = function SetGraphDescription(description) {
-    document.getElementById('graphDescription').innerHTML = description;
-}
-
-// Run custom javascript to refresh visualization
-//
-function Visualize(js) {
-    try {
-        eval(js);
-    } catch (e) {
-        console.log("Exception occurred during code execution: ")
-        console.log(e.message);
-        console.log(e.stack);
-        alert("Error: " + e.message);
-    }
+    var html = marked.parse(description);
+    document.getElementById('graphDescription').innerHTML = html;
 }
 
 // Build the file upload zone using FilePond
@@ -114,51 +145,158 @@ function BuildFileUploadZone() {
                 }
             });
 
-            // Initialize visualization with a chart from the sample
-            // data
+            // Visualize the chart
             //
-            var js = document.getElementById('codetext').innerText
-            Visualize(js);
+            Visualize();
         });
 
     return fp;
 }
 
-// Build the coding editor section
-// using CodeMirror
+// Ensure that the code for the graph is updated
 //
-function BuildCodeEditor() {
+function RefreshGraphCode() {
     // Copy over default code from the constants section in the html
     // into the textarea
     //
-    document.getElementById('codetext').innerHTML = document.getElementById('defaultCodeSnippet').innerHTML;
+    document.getElementById('codetext').innerHTML =
+        `/* The following code generate the chart above */
 
-    // Construct the code mirror instance from the textarea
-    //
-    return CodeMirror.fromTextArea(
-        document.getElementById('codetext'), {
-            lineNumbers: true,
-            mode: "javascript",
-            indentWithTabs: true,
-            tabSize: 4,
-            indentUnit: 4,
-            viewportMargin: 20,
-            lineWrapping: true
-        }
-    );
+new LineChart({
+    // Which data to plot
+    data: ['${window.configuration.dataset}'],
+    // Height & Width of the chart
+    width: ${window.configuration.width},
+    height: ${window.configuration.height},
+    // Which div to target
+    target: '.visualization',
+    // Key to access X values
+    xAccessor: '${window.configuration.xAccessor}',
+    // Key to access Y values
+    yAccessor: '${window.configuration.yAccessor}',
+    // Show area under line?
+    area: ${window.configuration.area},
+    // Customize x-axis
+    xAxis: ${JSON.stringify(window.configuration.xAxis)},
+    // Customize y-axis
+    yAxis: ${JSON.stringify(window.configuration.yAxis)},
+    // Customize colors of lines
+    colors: ${JSON.stringify(window.configuration.colors)},
+    // Highlight values along the x-axis
+    markers: ${JSON.stringify(window.configuration.markers)},
+    // Highlight values along the y-axis
+    baselines: ${JSON.stringify(window.configuration.baselines)},
+    // Describe each line
+    legend: ${JSON.stringify(window.configuration.legend)},
+});
+`;
+
+    // Re-highlight the code
+    hljs.highlightAll();
 }
 
-// Set up the visualization panel
+// Update the chart, and the chart-code blurb
 //
-function BuildVisualizationPanel() {
+function Visualize() {
+    var options = JSON.parse(JSON.stringify(window.configuration));
+    options.data = [window.getDataset(options.dataset).data];
+    options.target = ".visualization";
+
+    window.setGraphDescription(options.description);
+    window.setGraphTitle(options.title);
+
+    new LineChart(options);
+
+    // Refresh the code blurb
+    //
+    RefreshGraphCode();
+}
+
+// Set up the graph-code-blurb panel
+//
+function BuildGraphCodeBlurbPanel() {
     // Set up the refresh button
     // to read the code, and refresh visualization
     //
-    $('#refreshVisualization').on('click', function() {
-        var js = window.cm.getDoc().getValue();
-        Visualize(js);
+    $('#showCode').on('click', function() {
+        if (window.isCodeShowing) {
+            // Code snippet is showing - hide it
+            $('#showCode')[0].innerText = '> Show Code';
+            $('#codeSnippet').fadeOut();
+            window.isCodeShowing = false;
+        } else {
+            // Code snippet is hidden - show it
+            $('#showCode')[0].innerText = '< Hide Code';
+            $('#codeSnippet').fadeIn();
+            window.isCodeShowing = true;
+        }
         return false;
     });
+}
+
+// Auto-generate an editable json-table
+function CreateGraphBuilderTable() {
+
+    // Start building the table
+    var str = '<table class="objecttable">';
+    str += "<tr><th>Name</th><th>Value</th>"
+
+    // For each key-value pair in the object
+    // we need to construct a row in the table
+    //
+    var keyVals = Object.entries(window.configuration);
+    keyVals.forEach(element => {
+        var key = element[0];
+        var val = element[1];
+        var type = typeof val;
+
+        var valCell = `<td id="val${key}">${val}</td>`;
+        if (type == "object") {
+            // We don't handle nested objects
+            // If we have an object, just flatten it 
+            // into JSON, and display it.
+            //
+            val = JSON.stringify(val);
+            valCell = `<td class="objectcode" id="val${key}">${val}</td>`;
+        }
+
+        str += `<tr><td><code id="key${key}">${key}</code></td>${valCell}</tr>`
+    });
+    str += "</table>"
+
+    // Update the DOM
+    document.getElementById("graphBuilderForm").innerHTML = str;
+
+    // Post-processing:
+    //  -> Adding tooltips to the DOM elements
+    //  -> Setting up callbacks for editable `value` cells
+    //
+    keyVals.forEach(element => {
+        var key = element[0];
+
+        // Add tooltips to the key
+        tippy(`#key${key}`, {
+            content: window.configDescription[key],
+            theme: 'light-border',
+        });
+
+        // Make the values editable
+        $(`#val${key}`).editable({
+            save: function(content) {
+                var newContent = $(`#val${key}`)[0].innerText;
+                var type = typeof window.configuration[key];
+                if (type == "object") {
+                    newContent = JSON.parse(newContent);
+                }
+
+                // Update configuration
+                window.configuration[key] = newContent;
+
+                // Refresh chart
+                Visualize();
+            },
+        });
+    })
 }
 
 
@@ -179,11 +317,16 @@ function BuildVisualizationPanel() {
     //
     window.fileUploadZone = BuildFileUploadZone();
 
-    // Build code editor
+    // Create the graph builder table
     //
-    window.cm = BuildCodeEditor();
+    CreateGraphBuilderTable();
 
-    // Build visualization panel
+    // Create the graph builder
     //
-    BuildVisualizationPanel();
+    BuildGraphCodeBlurbPanel();
+
+    // Hide the graph code snippet
+    //
+    $('#codeSnippet').hide();
+    window.isCodeShowing = false;
 })();
